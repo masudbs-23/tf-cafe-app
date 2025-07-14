e1b550d7b25bfbe72ede6c2b06f3c959936c107a
MasudBS-1458 <masud.rana@brainstation-23.com>
2025-07-14 16:05:21 +0600

    checkout screen designed

--- android/app/src/main/java/com/awesomeproject/MainApplication.kt
+++ android/app/src/main/java/com/awesomeproject/MainApplication.kt
@@ -9,9 +9,7 @@ import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
-import org.devio.rn.splashscreen.SplashScreenReactPackage;
-// react-native-splash-screen < 0.3.1
-import com.cboy.rn.splashscreen.SplashScreenReactPackage;
+iimport com.oblador.vectoricons.VectorIconsPackage;
 class MainApplication : Application(), ReactApplication {
 
   override val reactNativeHost: ReactNativeHost =
@@ -20,6 +18,7 @@ class MainApplication : Application(), ReactApplication {
             PackageList(this).packages.apply {
               // Packages that cannot be autolinked yet can be added manually here, for example:
               // add(MyReactNativePackage())
+              new VectorIconsPackage()
             }
 
         override fun getJSMainModuleName(): String = "index"
